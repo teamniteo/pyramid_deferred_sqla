10c0287ab90a093abe345332046460b0a9615d0e
Domen Kožar <domen@dev.si>
2018-07-11 17:18:10 +0200

    add a test for stock include

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,4 +1,8 @@
-
+from pyramid.testing import testConfig
 
 def test_imports():
     import pyramid_deferred_sqla
+
+def test_stock_config():
+    with testConfig() as config:
+        config.include('pyramid_deferred_sqla')
