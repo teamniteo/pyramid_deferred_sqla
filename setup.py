221e48406fab0f007945d0d5a96b8eb77ead8519
Mandar Vaze <mandarvaze@gmail.com>
2019-08-26 22:52:20 +0530

    Explicit package name in package_data in setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     keywords="pyramid sqlalchemy",
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
     package_data={
-        '': ['LICENSE', 'py.typed'],
+        'pyramid_deferred_sqla': ['LICENSE', 'py.typed'],
     },
     include_package_data=True,
     install_requires=[
